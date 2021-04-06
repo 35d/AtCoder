487f25df377fc28343c6072d26bef569c3bbae7a
35d <xxx35d@gmail.com>
2021-04-06 11:24:26 +0900

    ✨ tutorial

---
+++ 2021-04-06-01.py
@@ -0,0 +1,13 @@
+# Welcome to AtCoder
+# https://atcoder.jp/contests/abs/tasks/practice_1
+
+# 整数の入力
+a = int(input())
+# スペース区切りの整数の入力
+b, c = map(int, input().split())
+
+# 文字列の入力
+s = input()
+
+# 出力
+print("{} {}".format(a+b+c, s))
