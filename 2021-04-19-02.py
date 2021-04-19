1e3cbc23502da87f87925be64ca832de55875f4f
35d <xxx35d@gmail.com>
2021-04-20 00:53:08 +0900

    ✨ Otoshidama AC

--- 2021-04-19-02.py
+++ 2021-04-19-02.py
@@ -12,11 +12,33 @@ answer = '-1 -1 -1'
 # A 10,000円札の枚数
 # B 5,000円札の枚数
 # C 1,000円札の枚数
-for A in range(N + 1):
-    for B in range(N + 1):
-        for C in range(N + 1):
-            # print(A, B, C, 10000 * A + 5000 * B + 1000 * C, A + B + C, 10000 * A + 5000 * B + 1000 * C == Y, A + B + C == N)
-            if 10000 * A + 5000 * B + 1000 * C == Y and A + B + C == N:
-                answer = str(A) + " " + str(B) + " " + str(C)
+
+# パフォーマンスが厳しい
+# for A in range(N + 1):
+#     # 1,0000円札で総額を超えたらもう組み合わせはほかにないので break する
+#     if 10000 * A > Y:
+#         break
+#     for B in reversed(range(N + 1 - A)):
+#         C = N - A - B
+#         print(A, B, C, 10000 * A + 5000 * B + 1000 * C, A + B + C,
+#               10000 * A + 5000 * B + 1000 * C == Y, A + B + C == N)
+#         if 10000 * A + 5000 * B + 1000 * C == Y:
+#             answer = str(A) + " " + str(B) + " " + str(C)
+#             break
+
+for A in reversed(range(N + 1)):
+    if A * 10000 > Y:
+        continue
+    if answer != '-1 -1 -1':  # 答えが1つでも見つかったらもう計算不要なのでループを抜ける
+        break
+    for B in reversed(range(N + 1 - A)):
+        if A * 10000 + B * 5000 > Y:
+            continue
+        C = N - A - B
+        sum = 10000 * A + 5000 * B + 1000 * C
+        # print(A, B, C, sum, sum == Y)
+        if sum == Y:
+            answer = str(A) + " " + str(B) + " " + str(C)
+            break
 
 print(answer)
